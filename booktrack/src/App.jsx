cdc40331c14eab8c554fd3fe820122dde58c4a56
shaharyaaransari <shaharyaaransari786@gmail.com>
2024-02-01 14:46:22 +0530

    login

---
+++ booktrack/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Login } from './pages/Login'
+
+function App() {
+ 
+
+  return (
+   <div>
+     <Login/>
+   </div>
+  )
+}
+
+export default App
---
+++ booktrack/src/pages/Login.jsx
@@ -0,0 +1,137 @@
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+export const Login = () => {
+  const initialValue = { name: "", email: "", password: "", role: "" };
+  const [details, setDetails] = useState(initialValue);
+  const [formError, setFormError] = useState({});
+  const [show, setShow] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDetails({ ...details, [name]: value });
+  }
+
+  const handleRegister = () => {
+    setFormError(validate(details));
+  
+    if (details.email && details.name && details.password && details.role) {
+        axios.post(`http://localhost:8080/user/signup`,details)
+        .then((res)=>{
+            console.log(res)
+               if(res.data.msg){
+                withReactContent(Swal).fire({
+                    title: <i>Register successful.</i>,
+                  })
+               }else{
+                withReactContent(Swal).fire({
+                    title: <i>Register successful.</i>,
+                  })
+               }
+        })
+        .catch((err)=>{
+         console.log(err)
+        })
+     
+      setShow(!show);
+       
+    }
+    setDetails(initialValue);
+  }
+
+  const validate = (value) => {
+    const error = {};
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!value.name) {
+      error.name = "name is required!";
+    }
+    if (!value.email) {
+      error.email = "Email is required!";
+    }
+    if (!value.role) {
+      error.role = "Role is required!";
+    }
+    if (!value.password) {
+      error.password = "Password is required!";
+    } else if (!regex.test(value.password)) {
+      error.password = "Please enter a valid password!";
+    }
+    return error;
+  }
+
+  const handleChangeLogin = (e) => {
+    const { name, value } = e.target;
+    setDetails({ ...details, [name]: value });
+  }
+
+  const handleLogin = () => {
+   
+   
+     
+      setFormError(validate(details));
+
+      if ( details.email && details.password) {
+        axios.post(`http://localhost:8080/user/login`,details)
+        .then((res)=>{
+            console.log(res)
+              if(res.data.token){
+                withReactContent(Swal).fire({
+                    title: <i>Login successful.</i>,
+                  })
+              }
+              else {
+                withReactContent(Swal).fire({
+                  title: <i>Invalid email or password.</i>,
+                })
+              }
+        })
+        .catch((err)=>{
+         console.log(err)
+        })
+        
+     
+      } 
+   
+      withReactContent(Swal).fire({
+        title: <i>User not found. Please register.</i>,
+      })
+    
+    setDetails(initialValue);
+  }
+
+  return (
+    <div>
+      <div className="back">
+        {show ? <>
+          <h2>Sign Up</h2>
+          <img src="https://www.bonanzaonline.com/images/img_avatar.png" alt="avatar" />
+          <label htmlFor="uname"><b>name</b></label>
+          <input type="text" name="name" value={details.name} onChange={handleChange} placeholder="Enter name" />
+          <p>{formError.name}</p>
+          <label htmlFor="email"><b>Email</b></label>
+          <input type="email" name="email" value={details.email} onChange={handleChange} placeholder="Enter Email" />
+          <p>{formError.email}</p>
+          <label htmlFor="role"><b>Role</b></label>
+          <input type="text" name="role" value={details.role} onChange={handleChange} placeholder="Enter Role" />
+          <p>{formError.role}</p>
+          <label htmlFor="psw"><b>Password</b></label>
+          <input type="password" name="password" value={details.password} onChange={handleChange} placeholder="Enter password" />
+          <p>{formError.password}</p>
+          <button onClick={handleRegister}>Register</button>
+        </> : <>
+          <h2>Login</h2>
+          <img src="https://www.bonanzaonline.com/images/img_avatar.png" alt="avatar" />
+          <label htmlFor="email"><b>Email</b></label>
+          <input type="email" name="email" value={details.email} onChange={handleChangeLogin} placeholder="Enter Email" />
+          <p>{formError.email}</p>
+          <label htmlFor="password"><b>Password</b></label>
+          <input type="password" name="password" value={details.password} onChange={handleChangeLogin} placeholder="Enter password" />
+          <button onClick={handleLogin}>Login</button>
+          <button onClick={() => setShow(!show)}>Register</button>
+        </>}
+      </div>
+    </div>
+  )
+}
